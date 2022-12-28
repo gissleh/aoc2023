818a732d30d5e7729c28b53631cec7a87bd10da9
Gisle Aune <dev@gisle.me>
2022-12-28 18:04:25 +0100

    add And parser and find_first_parsable.

---
+++ src/parse/and.rs
@@ -0,0 +1,117 @@
+use std::marker::PhantomData;
+use crate::parse::{Parser, ParseResult};
+
+pub struct And<PL, TL, PR, TR> {
+    parse_left: PL,
+    parse_right: PR,
+    spooky_ghost: PhantomData<(TL, TR)>,
+}
+
+impl<'i, PL, TL, PR, TR> And<PL, TL, PR, TR> where PL: Parser<'i, TL>, PR: Parser<'i, TR> {
+    #[inline]
+    pub(crate) fn new(parse_left: PL, parse_right: PR) -> Self {
+        Self { parse_left, parse_right, spooky_ghost: Default::default() }
+    }
+}
+
+impl<PL, TL, PR, TR> Copy for And<PL, TL, PR, TR> where PL: Copy, PR: Copy {}
+
+impl<PL, TL, PR, TR> Clone for And<PL, TL, PR, TR> where PL: Clone, PR: Clone {
+    #[inline]
+    fn clone(&self) -> Self {
+        Self {
+            parse_left: self.parse_left.clone(),
+            parse_right: self.parse_right.clone(),
+            spooky_ghost: Default::default(),
+        }
+    }
+}
+
+impl<'i, PL, TL, PR, TR> Parser<'i, (TL, TR)> for And<PL, TL, PR, TR> where PL: Parser<'i, TL>, PR: Parser<'i, TR> {
+    fn parse(&self, input: &'i [u8]) -> ParseResult<'i, (TL, TR)> {
+        match self.parse_left.parse(input) {
+            ParseResult::Good(vl, input) => match self.parse_right.parse(input) {
+                ParseResult::Good(vr, input) => ParseResult::Good((vl, vr), input),
+                ParseResult::Bad(err) => ParseResult::Bad(err.wrap("Right in And failed", input))
+            },
+            ParseResult::Bad(err) => ParseResult::Bad(err.wrap("Left in And failed", input))
+        }
+    }
+}
+
+pub struct AndDiscard<PL, TL, PR, TR> {
+    parse_left: PL,
+    parse_right: PR,
+    spooky_ghost: PhantomData<(TL, TR)>,
+}
+
+impl<'i, PL, TL, PR, TR> AndDiscard<PL, TL, PR, TR> where PL: Parser<'i, TL>, PR: Parser<'i, TR> {
+    #[inline]
+    pub(crate) fn new(parse_left: PL, parse_right: PR) -> Self {
+        Self { parse_left, parse_right, spooky_ghost: Default::default() }
+    }
+}
+
+impl<PL, TL, PR, TR> Copy for AndDiscard<PL, TL, PR, TR> where PL: Copy, PR: Copy {}
+
+impl<PL, TL, PR, TR> Clone for AndDiscard<PL, TL, PR, TR> where PL: Clone, PR: Clone {
+    #[inline]
+    fn clone(&self) -> Self {
+        Self {
+            parse_left: self.parse_left.clone(),
+            parse_right: self.parse_right.clone(),
+            spooky_ghost: Default::default(),
+        }
+    }
+}
+
+
+impl<'i, PL, TL, PR, TR> Parser<'i, TL> for AndDiscard<PL, TL, PR, TR> where PL: Parser<'i, TL>, PR: Parser<'i, TR> {
+    fn parse(&self, input: &'i [u8]) -> ParseResult<'i, TL> {
+        match self.parse_left.parse(input) {
+            ParseResult::Good(vl, input) => match self.parse_right.parse(input) {
+                ParseResult::Good(_, input) => ParseResult::Good(vl, input),
+                ParseResult::Bad(err) => ParseResult::Bad(err.wrap("Right in And failed", input))
+            },
+            ParseResult::Bad(err) => ParseResult::Bad(err.wrap("Left in And failed", input))
+        }
+    }
+}
+
+pub struct AndReplace<PL, TL, PR, TR> {
+    parse_left: PL,
+    parse_right: PR,
+    spooky_ghost: PhantomData<(TL, TR)>,
+}
+
+impl<'i, PL, TL, PR, TR> AndReplace<PL, TL, PR, TR> where PL: Parser<'i, TL>, PR: Parser<'i, TR> {
+    #[inline]
+    pub(crate) fn new(parse_left: PL, parse_right: PR) -> Self {
+        Self { parse_left, parse_right, spooky_ghost: Default::default() }
+    }
+}
+
+impl<PL, TL, PR, TR> Copy for AndReplace<PL, TL, PR, TR> where PL: Copy, PR: Copy {}
+
+impl<PL, TL, PR, TR> Clone for AndReplace<PL, TL, PR, TR> where PL: Clone, PR: Clone {
+    #[inline]
+    fn clone(&self) -> Self {
+        Self {
+            parse_left: self.parse_left.clone(),
+            parse_right: self.parse_right.clone(),
+            spooky_ghost: Default::default(),
+        }
+    }
+}
+
+impl<'i, PL, TL, PR, TR> Parser<'i, TR> for AndReplace<PL, TL, PR, TR> where PL: Parser<'i, TL>, PR: Parser<'i, TR> {
+    fn parse(&self, input: &'i [u8]) -> ParseResult<'i, TR> {
+        match self.parse_left.parse(input) {
+            ParseResult::Good(_, input) => match self.parse_right.parse(input) {
+                ParseResult::Good(vr, input) => ParseResult::Good(vr, input),
+                ParseResult::Bad(err) => ParseResult::Bad(err.wrap("Right in And failed", input))
+            },
+            ParseResult::Bad(err) => ParseResult::Bad(err.wrap("Left in And failed", input))
+        }
+    }
+}
--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -1,16 +1,51 @@
 use std::fmt::{Display, Formatter};
 use repeat::{Repeat, RepeatDelimited};
 use skip::{Skip, SkipAll};
+use and::{And, AndReplace, AndDiscard};
 
 mod repeat;
 mod skip;
+mod and;
 mod int;
 
 pub use int::{signed_int, unsigned_int, digit};
 
-
 pub trait Parser<'i, T>: Sized + Copy {
     fn parse(&self, input: &'i [u8]) -> ParseResult<'i, T>;
+
+    /// Find the first parsable result in the input.
+    fn first_parsable_in(&self, input: &'i [u8]) -> ParseResult<'i, (T, usize)> {
+        let mut input = input;
+        let mut offset = 0;
+        while !input.is_empty() {
+            if let ParseResult::Good(v, next_input) = self.parse(input) {
+                return ParseResult::Good((v, offset), next_input);
+            }
+
+            input = &input[1..];
+            offset += 1;
+        }
+
+        ParseResult::Bad(ParseError::new("None were found", input))
+    }
+
+    /// And parses the current parser, and then the one to the right, returning both values.
+    /// You may want to use and_discard or and_instead to keep only one value.
+    fn and<PR, TR>(self, parse_right: PR) -> And<Self, T, PR, TR> where PR: Parser<'i, TR> {
+        And::new(self, parse_right)
+    }
+
+    /// And parses the current parser, and then the one to the right, returning both values.
+    /// You may want to use and_discard or and_instead to keep only one value.
+    fn and_instead<PR, TR>(self, parse_right: PR) -> AndReplace<Self, T, PR, TR> where PR: Parser<'i, TR> {
+        AndReplace::new(self, parse_right)
+    }
+
+    /// And parses the current parser, and then the one to the right, returning both values.
+    /// You may want to use and_discard or and_instead to keep only one value.
+    fn and_discard<PR, TR>(self, parse_right: PR) -> AndDiscard<Self, T, PR, TR> where PR: Parser<'i, TR> {
+        AndDiscard::new(self, parse_right)
+    }
 
     /// Repeat this parser until the collection fills or a non-first result fails.
     /// If the first result fails, it will fail as well.
@@ -22,31 +57,37 @@ pub trait Parser<'i, T>: Sized + Copy {
     /// * `ArrayVec<T, N>` – ArrayVec, will stop if full
     /// * `SmallVec<[T; N]>` – SmallVec, will be treated as vector
     /// * `(T, T) | (T, T, T) | (T, T, T, T)` – Collect it into a tuple, unfilled will have default value
+    #[inline]
     fn repeat<R>(self) -> Repeat<Self, T, R> {
         Repeat::new(self)
     }
 
     /// Same as repeat, but it requires a delimiter between values.
+    #[inline]
     fn repeat_delimited<R, TD, PD>(self, delimiter_parser: PD) -> RepeatDelimited<Self, T, PD, TD, R> where PD: Parser<'i, TD> {
         RepeatDelimited::new(self, delimiter_parser)
     }
 
     /// Same as repeat, but it requires a specified amount.
+    #[inline]
     fn repeat_n<R>(self, amount: usize) -> Repeat<Self, T, R> {
         Repeat::with_amount(self, amount)
     }
 
     /// Same as repeat_delimited, but it requires a specified amount.
+    #[inline]
     fn repeat_delimited_n<R, TD, PD>(self, delimiter_parser: PD, amount: usize) -> RepeatDelimited<Self, T, PD, TD, R> where PD: Parser<'i, TD> {
         RepeatDelimited::with_amount(self, delimiter_parser, amount)
     }
 
     /// Parse this, then skip what comes from parser2.
+    #[inline]
     fn then_skip<P2, T2>(self, parser: P2) -> Skip<Self, P2, T, T2> where P2: Parser<'i, T2> {
         Skip::new(self, parser)
     }
 
     /// Parse this, then skip until the second parses fails.
+    #[inline]
     fn then_skip_all<P2, T2>(self, parser: P2) -> SkipAll<Self, P2, T, T2> where P2: Parser<'i, T2> {
         SkipAll::new(self, parser)
     }
@@ -103,6 +144,14 @@ impl<'i> Parser<'i, u8> for u8 {
             ParseResult::Bad(ParseError::new("u8 not matched", input))
         }
     }
+
+    #[inline]
+    fn first_parsable_in(&self, input: &'i [u8]) -> ParseResult<'i, (u8, usize)> {
+        match input.iter().position(|v| *v == *self) {
+            Some(index) => ParseResult::Good((*self, index), &input[index + 1..]),
+            None => ParseResult::Bad(ParseError::new("Byte not found in input", input)),
+        }
+    }
 }
 
 impl<'i, 's> Parser<'i, &'i [u8]> for &'s [u8] {
@@ -117,6 +166,16 @@ impl<'i, 's> Parser<'i, &'i [u8]> for &'s [u8] {
             }
         } else {
             ParseResult::Bad(ParseError::new("String is too short", input))
+        }
+    }
+
+    #[inline]
+    fn first_parsable_in(&self, input: &'i [u8]) -> ParseResult<'i, (&'i [u8], usize)> {
+        match input.windows(self.len()).enumerate().find(|(_, w)| w == self) {
+            Some((index, data)) => ParseResult::Good(
+                (data, index), &input[index + self.len()..],
+            ),
+            None => ParseResult::Bad(ParseError::new("Byte slice not found in input", input)),
         }
     }
 }
@@ -135,11 +194,67 @@ impl<'i, 's, const N: usize> Parser<'i, &'i [u8]> for &'s [u8; N] {
             ParseResult::Bad(ParseError::new("String is too short", input))
         }
     }
+
+    #[inline]
+    fn first_parsable_in(&self, input: &'i [u8]) -> ParseResult<'i, (&'i [u8], usize)> {
+        match input.windows(N).enumerate().find(|(_, w)| *w == self.as_slice()) {
+            Some((index, data)) => ParseResult::Good((data, index), &input[index + N..]),
+            None => ParseResult::Bad(ParseError::new("Byte slice not found in input", input)),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Everything;
+
+impl<'i> Parser<'i, &'i [u8]> for Everything {
+    fn parse(&self, input: &'i [u8]) -> ParseResult<'i, &'i [u8]> {
+        let len = input.len();
+        if len > 0 {
+            ParseResult::Good(input, &input[len..])
+        } else {
+            ParseResult::Bad(ParseError::new("Nothing is the only thing that does not match Everything", input))
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Anything;
+
+impl<'i> Parser<'i, &'i [u8]> for Anything {
+    fn parse(&self, input: &'i [u8]) -> ParseResult<'i, &'i [u8]> {
+        let len = input.len();
+        ParseResult::Good(input, &input[len..])
+    }
+}
+
+pub fn everything<'i>() -> impl Parser<'i, &'i [u8]> {
+    Everything
+}
+
+pub fn anything<'i>() -> impl Parser<'i, &'i [u8]> {
+    Anything
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn parser_find_first_parsable_works_for_the_basics() {
+        assert_eq!(
+            b'H'.first_parsable_in(b"Hello, World"),
+            ParseResult::Good((b'H', 0), b"ello, World"),
+        );
+        assert_eq!(
+            b' '.first_parsable_in(b"Hello, World"),
+            ParseResult::Good((b' ', 6), b"World"),
+        );
+        assert_eq!(
+            signed_int::<i32>().first_parsable_in(b"The number is -954923166!"),
+            ParseResult::Good((-954923166, 14), b"!"),
+        );
+    }
 
     #[test]
     fn byte_match_works() {
