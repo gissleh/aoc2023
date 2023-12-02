9893ec46fe3d01b4b5bcb8f124e1c021673f4b66
Gisle Aune <dev@gisle.me>
2023-12-02 14:13:53 +0100

    day2: faster and more readable parse code.

--- src/2023/day02.rs
+++ src/2023/day02.rs
@@ -1,12 +1,12 @@
 use common::aoc::Day;
 use common::parse;
-use common::parse::{any_byte, Parser};
+use common::parse::{any_byte, choice, Parser};
 
 pub fn main(day: &mut Day, input: &[u8]) {
-    let list = day.prep("Parse", || parse_list(input));
+    let list = day.prep("Parse", || parse_games(input));
 
-    day.note("Input games", list.len());
-    day.note("Amount of cubes", list.iter().map(|g| g.cubes.len()).sum::<usize>());
+    day.note("Amount of games", list.len());
+    day.note("Amount of ops", list.iter().map(|g| g.cubes.len()).sum::<usize>());
 
     day.part("Part 1", || p1(&list));
     day.part("Part 2", || p2(&list));
@@ -27,18 +27,17 @@ fn p2(input: &[Game]) -> u32 {
         .sum()
 }
 
-fn parse_list(input: &[u8]) -> Vec<Game> {
+fn parse_games(input: &[u8]) -> Vec<Game> {
     b"Game "
         .and_instead(parse::unsigned_int())
         .and_discard(b": ")
         .and(parse::unsigned_int::<u8>()
             .and_discard(b' ')
-            .and(parse::word().map(|w| match w[0] {
-                b'r' => 0u8,
-                b'g' => 1u8,
-                b'b' => 2u8,
-                _ => panic!("Unknown word {}", String::from_utf8_lossy(w))
-            }))
+            .and(choice((
+                b"red".map_to(0),
+                b"green".map_to(1),
+                b"blue".map_to(2),
+            )))
             .and(any_byte().map(|d| d != b',').or_return(true))
             .map(|((amount, color), end_of_round)| (amount, color, end_of_round))
             .then_skip(b' ')
